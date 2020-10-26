60c979ed0ea297d2223aa9ffe1465015f462a27c
unknown <you@example.com>
2020-10-26 20:31:16 +0600

    Updates

---
+++ precache-manifest.af5f6e074904f900b5102bf3b8efb11b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa822541acf413e7ec5d5322ecfdd58",
+    "url": "/instagramclone/index.html"
+  },
+  {
+    "revision": "63984e4329347528fe39",
+    "url": "/instagramclone/static/css/main.8042033c.chunk.css"
+  },
+  {
+    "revision": "04bf0158b425ba04f6aa",
+    "url": "/instagramclone/static/js/2.e64ba044.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/instagramclone/static/js/2.e64ba044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63984e4329347528fe39",
+    "url": "/instagramclone/static/js/main.8b78d28b.chunk.js"
+  },
+  {
+    "revision": "e587a927853d25388668",
+    "url": "/instagramclone/static/js/runtime-main.0d44a490.js"
+  }
+]);
